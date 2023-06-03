336702b44504ccb1abc6f92b83bc369cbe13729c
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-03 16:49:35 +0800

    Almost done. Just sizing and stuff left.

--- src/Solutions/ResultsSummaryComponent/index.tsx
+++ src/Solutions/ResultsSummaryComponent/index.tsx
@@ -1,35 +1,55 @@
 import data from './data.json'
-import './styles.scss'
+import './scss/styles.scss'
+
+interface ISummaryItemColors {
+    [key: string]: string[];
+}
+const summaryItemColors:ISummaryItemColors = {
+    Reaction: [ 'rgb(255, 87, 87)', 'rgb(255, 87, 87, 0.1)' ],
+    Memory: [ 'rgb(255, 176, 31)', 'rgb(255, 176, 31, 0.1)' ],
+    Verbal: [ 'rgb(0, 189, 145)', 'rgb(0, 189, 145, 0.1)' ],
+    Visual: [ 'rgb(17, 37, 212)', 'rgb(17, 37, 212, 0.1)' ],
+} ;
 
 const ResultsSummaryComponent = () => {
     return (
-        <div id='results-summary'>
-            <header>
-                <h1>Your Result</h1>
-                <div>
-                    <span><strong>76</strong> of 100</span>
-                    <div>
-                        <strong>Great</strong>
-                        <p>You scored higher than 65% of the people who have taken these tests.</p>
+        <div className='container'>
+            <div id='results-summary'>
+                <header>
+                    <h1>Your Result</h1>
+                    <div className='result-info'>
+                        <div className='result-score'>
+                            <div>
+                                <strong>76</strong> <span>of 100</span>
+                            </div>
+                        </div>
+                        <div className='result-description'>
+                            <p className='result-status'><b>Great</b></p>
+                            <p className='result-feedback'>You scored higher than 65% of the people who have taken these tests.</p>
+                        </div>
                     </div>
-                </div>
-            </header>
-            <section>
-                <h2>Summary</h2>
-                { data.map((d) => 
-                    <div key={d.category}>
-                        <label htmlFor="reaction">
-                            <img src={d.icon} alt="" />
-                            {d.category}
-                        </label>
+                </header>
+                <section>
+                    <h2>Summary</h2>
+                    <div className='summary-items'>
+                        <ul>
+                            { data.map((d, i) => 
+                                <li key={d.category} style={{ background: summaryItemColors[d.category][1] }} >
+                                    <span style={{ color: summaryItemColors[d.category][0] }}>
+                                        <img src={d.icon} alt={`${d.category} icon`} />
+                                        {d.category}
+                                    </span>
+                                    <div id={`summaryitem-${i}`}><em>{d.score}</em> / 100</div>
+                                </li>
+                            ) }
+                        </ul>
+                    </div>
 
-                        <div id="reaction"><em>{d.score}</em> / 100</div>
-                    </div>
-                ) }
-            </section>
-            <p>
-                <button>Continue</button>
-            </p>
+                    <p className='button-container'>
+                        <button>Continue</button>
+                    </p>
+                </section>
+            </div>
         </div>
     )
 }
