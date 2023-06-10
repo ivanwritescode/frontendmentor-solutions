f4a30b4295905d8cdaf375791a5fa3094ac3cfae
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-10 17:05:12 +0800

    Removed react-router-dom support

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import './App.css'
 import ResultsSummaryComponent from './Solutions/ResultsSummaryComponent'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <ResultsSummaryComponent /> 
-  },
-  {
-    path: "/1",
-    element: <ResultsSummaryComponent />
-  }
-],
-{
-  basename: "/frontendmentor-solutions"
-})
 
 function App() {
 
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <ResultsSummaryComponent />
   )
 }
 
